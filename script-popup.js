305cbd5cafe2989be4c88ac743e4166961224e49
danacaletkova <164472411+danacaletkova@users.noreply.github.com>
2024-05-23 19:32:35 +0800

    Apply changes to text as per clients' request

--- script-popup.js
+++ script-popup.js
@@ -1,7 +1,7 @@
 function showPopUp() {
   let popUpElement = document.querySelector("#pop-up");
   popUpElement.classList.remove("hidden");
-  popUpElement.innerHTML = `<div><div>A magyar nyelvű információkért kattintson a Nori névre</div><div class="line"></div><div>Pro informace v češtině klikněte na jméno Tom</div><i class="fa-solid fa-square-xmark fa-2xl" id="cross"></i></div>`;
+  popUpElement.innerHTML = `<div><div>A magyar nyelvű információkért kattints a Nori névre</div><div class="line"></div><div>Pro informace v češtině klikněte na jméno Tom</div><i class="fa-solid fa-square-xmark fa-2xl" id="cross"></i></div>`;
 
   let crossButton = document.querySelector("#cross");
   crossButton.addEventListener("click", function () {
